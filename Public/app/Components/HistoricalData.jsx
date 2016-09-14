66b74cc9a7a925f944c1d11eebdf3d7c22a028e2
shea12 <cshearun12@gmail.com>
2016-09-14 15:34:39 -0700

    add url template for api call, componentDidMount fn

--- Public/app/Components/HistoricalData.jsx
+++ Public/app/Components/HistoricalData.jsx
@@ -6,21 +6,30 @@ React component for historical bitcoin data
 
 class HistoricalData extends React.Component {
   constructor(props) {
-  	super(props);
+    super(props);
 
-  	this.state = {
-  	  date: {
-  	  	text: 'Choose Date'
-  	  }
-  	};
+    this.state = {
+    
+    };
   }
 
   componentDidMount() {
-
+  	//when the chooseDate button is clicked,
+    $('.chooseDate').on('click', function(data) {
+      //set selectedDate to the value the user entered
+      var selectedDate = $(data.target).text();
+      //call getHistoricalPriceData with selectedDate
+      window.getHistoricalPriceData(selectedDate);
+    });
   }
 
   render() {
-
+    return (
+      <div className='panel panel-primary'>
+        <div className='panel-heading'>Historical Data</div>
+        <div className='panel-body histData'>put data here</div>
+      </div>
+    );
   }
 
 };
--- Public/app/lib/histPriceData.js
+++ Public/app/lib/histPriceData.js
@@ -6,16 +6,17 @@ This file grabs historical bitcoin price data from CoinDesk API
 */
 
 window.histPriceData = {};
-
+var url_stub = 'http(s)://api.coindesk.com/v1/bpi/historical/close.json' 
 //query CoinDesk API for data, render data in historical data component
 //this is called when user clicks on component
-var getHistoricalPriceData = function() {
+var getHistoricalPriceData = function(date) {
   $.ajax({
-      url: 'http(s)://api.coindesk.com/v1/bpi/currentprice.json',
+  	  // example end point for date range ?start=2013-09-01&end=2013-09-05
+      url: url_stub + '?start=' + range_start + '&end=' + range_end,
       method: 'GET',
       success: (data) => {
         var histData = JSON.parse(data);
-        $('histData').empty();
+        $('.histData').empty();
         $('.histData').append('<h3 class="text-center">Historical Bitcoin Prices</h3><br>');
         $('.histData').append('<div>' + histData + '</div>');
       },
@@ -25,4 +26,3 @@ var getHistoricalPriceData = function() {
 
 //Adds it to the window
 window.getHistoricalPriceData = getHistoricalPriceData;
-
