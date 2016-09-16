21ce727cb7fb6681039adceedc00af6f378c4110
shea12 <cshearun12@gmail.com>
2016-09-15 18:56:33 -0700

    add fields for user entered date

--- Public/app/Components/HistoricalData.jsx
+++ Public/app/Components/HistoricalData.jsx
@@ -15,10 +15,20 @@ class HistoricalData extends React.Component {
   	//when the chooseDate button is clicked,
     $('.chooseDate').on('click', function(data) {
       //set selectedDate to the value the user entered
-      var selectedDate = $(data.target).text();
+      var start_day = $('.dayForm').val();
+      var start_month = $('.monthForm').val();
+      var start_year = $('.yearForm').val();
       //call getHistoricalPriceData with selectedDate
-      window.getHistoricalPriceData(selectedDate);
+      //2013-09-01
+      var start_date = start_year + '-' + start_month + '-' + start_day;
+      var end_date = '2014-01-02';
+      // console.log('combined start_date: ', start_date);
+      window.getHistoricalPriceData(start_date, end_date, function(data) {
+      	console.log('start_date price: ', data.bpi[start_date]);
+      	// console.log('end_date price: ', data.bpi[end_date]);
 
+      	// console.log('data: ', data);
+      });
       // $('.histData').append('<div>' + histData + '</div>');
     });
   }
@@ -28,11 +38,24 @@ class HistoricalData extends React.Component {
       <div className="panel panel-primary">
         <div className="panel-heading">Historical Prices</div>
         <div className="panel-body">
-        Look up historical bitcoin prices
-          <div className="form-group">
-            <textarea className="form-control address"></textarea>
-            <button className="btn btn-primary chooseDate">Look Up</button>
-          </div>
+          <form className="form-group">
+	          <div>
+	          	<label>DD</label>
+	            <input name='day' type='text' className="dayForm"/>
+	          </div>
+              <div>
+	          	<label>MM</label>
+	            <input name='month' type='text' className="monthForm"/>
+	          </div>
+	          <div>
+	          	<label>YYYY</label>
+	            <input name='year' type='text' className="yearForm"/>
+	          </div>
+          </form>
+        
+          <div>
+      		<button type='submit' value='Submit' className="btn btn-primary chooseDate">Submit</button>
+      	  </div>
         </div>
       </div>
     );
--- Public/app/lib/histPriceData.js
+++ Public/app/lib/histPriceData.js
@@ -9,20 +9,18 @@ window.histPriceData = {};
 var url_stub = 'http://api.coindesk.com/v1/bpi/historical/close.json' 
 //query CoinDesk API for data, render data in historical data component
 //this is called when user clicks on component
-var getHistoricalPriceData = function(date) {
-  var range_start = '2013-09-01';
-  var range_end = '2013-09-05';
+var getHistoricalPriceData = function(start_date, end_date, cb) {
+  // console.log('req', url_stub + start_date);
+  // end_date = '2014-01-02';
+  //first available start date '2010-07-18'
   $.ajax({
   	  // example end point for date range ?start=2013-09-01&end=2013-09-05
       // url: url_stub + '?start=' + range_start + '&end=' + range_end,
-      url: url_stub + '?start=2013-09-01&end=2013-09-05',
+      url: url_stub + '?start=' + start_date,
       method: 'GET',
       success: (data) => {
-        // var histData = JSON.parse(data);
-        // $('.histData').empty();
-        // $('.histData').append('<h3 class="text-center">Historical Bitcoin Prices</h3><br>');
-        // $('.histData').append('<div>' + histData + '</div>');
-        console.log('data returned from CoinDesk: ', data);
+        // console.log('CoinDesk response: ', data);
+        cb(JSON.parse(data));
       },
       error: (error) => console.log('An error occurred while trying to get historical data: ', error)
     });
@@ -31,3 +29,9 @@ var getHistoricalPriceData = function(date) {
 // getHistoricalPriceData();
 //Adds it to the window
 window.getHistoricalPriceData = getHistoricalPriceData;
+
+
+// var histData = JSON.parse(data);
+// $('.histData').empty();
+// $('.histData').append('<h3 class="text-center">Historical Bitcoin Prices</h3><br>');
+// $('.histData').append('<div>' + histData + '</div>');
