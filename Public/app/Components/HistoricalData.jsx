6c91d9a3207b9f9dbf307818d66e341922ea5337
shea12 <cshearun12@gmail.com>
2016-09-16 20:47:38 -0700

    render bitcoin price, need to do date validation

--- Public/app/Components/HistoricalData.jsx
+++ Public/app/Components/HistoricalData.jsx
@@ -3,36 +3,25 @@ React component for historical bitcoin data
 */ 
 
 class HistoricalData extends React.Component {
-  // constructor(props) {
-  //   super(props);
-
-  //   this.state = {
-    
-  //   };
-  // }
 
   componentDidMount() {
   	//when the chooseDate button is clicked,
     $(".chooseDate").on("click", function(data) {
-      //set selectedDate to the value the user entered
+      //set vars for date values the user entered
       var start_day = $("#dayForm").val();
       var start_month = $("#monthForm").val();
       var start_year = $("#yearForm").val();
-      //call getHistoricalPriceData with selectedDate
-      //2013-09-01
       var start_date = start_year + "-" + start_month + "-" + start_day;
-      var end_date = "2014-01-02";
-      // console.log("combined start_date: ", start_date);
+      var end_date;
+
+      /*
+		Need to do validation on dates!!
+      */
+
       window.getHistoricalPriceData(start_date, end_date, function(data) {
-      	console.log("data calledback from req", data);
-      	console.log("start_date price: ", data.bpi[start_date]);
-      	// console.log("end_date price: ", data.bpi[end_date]);
-      	$("#resultsText").text(data.bpi[start_date]);
-      	alert(data.bpi[start_date]);
-      	// console.log("data: ", data);
+      	$("#resultsText").text('$'+Math.round(data.bpi[start_date] * 100) / 100);
       });
-      // $(".histData").append("<div>" + histData + "</div>");
-    });
+     });
   }
 
   render() {
@@ -41,7 +30,7 @@ class HistoricalData extends React.Component {
         <div className="panel-heading">Historical Prices</div>
         <div className="panel-body">
           <div className="histText"> 
-            Enter a date for that day's bitcoin price. Note, first available price is 2010-07-18.
+            Enter a date for that day's bitcoin price. Note, earliest available data is 2010-07-18.
           </div>
           <form className="form-group">
           	<span style={{display:'inline-block'}}>
@@ -56,16 +45,14 @@ class HistoricalData extends React.Component {
 	          	<label className="dateLabel">DD</label>
 	            <input name="day" maxLength="2" type="text" className="dateForm" id="dayForm"/>
 	        </span>
-        
-            <span style={{display:'inline-block'}}>
-      		  <button type="submit" value="Submit" className="btn btn-primary chooseDate">Submit</button>
-      	    </span>
-
       	    <span id="resultsBox" style={{display:'inline-block'}}>
       	  	  <text id="resultsText"></text>
       	    </span>
 
           </form>
+          <span style={{display:'inline-block'}}>
+  		    <button type="submit" value="Submit" className="btn btn-primary chooseDate">Submit</button>
+  	      </span>
         </div>
       </div>
     );
